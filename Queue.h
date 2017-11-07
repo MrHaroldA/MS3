a31643e7aef18467c34c400f4439e582e413a336
MrHaroldA <harold@sait.nl>
2017-11-08 00:18:01 +0100

    Add queue protection!

--- Queue.h
+++ Queue.h
@@ -13,54 +13,87 @@ typedef struct {
 
 class Queue {
     private:
-        queueItem queue[MS3_QUEUE_SIZE] = {};
-        uint8_t readPointer = 0;
+        queueItem items[MS3_QUEUE_SIZE] = {};
         uint8_t writePointer = 0;
+
+        /**
+         * Shift an item off the beginning of the queue.
+         */
+        queueItem shift() {
+            queueItem item = Queue::items[0];
+
+            // Move all queued items one up.
+            for (uint8_t i = 1; i < Queue::writePointer; i++) {
+                Queue::items[i - 1] = Queue::items[i];
+            }
+            Queue::writePointer--;
+
+            return item;
+        }
 
     public:
 
         /**
-         * Get the current queue item. This always returns an item, even when the queue is considered empty.
+         * Get the first queued item.
          */
         bool read(queueItem &item) {
             if (Queue::isEmpty()) {
                 return false;
             }
 
-            MS3_DEBUG(F("Handle queue: "));
-            MS3_DEBUGLN(readPointer);
+            item = Queue::shift();
 
-            item = queue[readPointer];
-
-            // Wrap pointer.
-            readPointer = (readPointer < MS3_QUEUE_SIZE - 1) ? readPointer + 1 : 0;
+            MS3_DEBUG(F("Picked up item 0 from the queue: "));
+            MS3_DEBUG_AS(item.address, HEX);
+            MS3_DEBUG(F(" / 0x"));
+            MS3_DEBUG_AS(item.operation, HEX);
+            MS3_DEBUG(F(" / 0x"));
+            MS3_DEBUG_AS(item.data, HEX);
+            MS3_DEBUGLN();
 
             return true;
         }
 
         /**
          * Add an item to the queue.
-         *
-         * @TODO: add protection against overwriting items.
          */
         void write(uint32_t address, uint8_t data, uint8_t dataLength, uint8_t operation) {
-            MS3_DEBUG(F("Add to queue: "));
-            MS3_DEBUGLN(writePointer);
+            if (Queue::writePointer == MS3_QUEUE_SIZE) {
+                MS3_DEBUG(F("*** Queue is full! Discarding item 0: 0x"));
+                MS3_DEBUG_AS(Queue::items[0].address, HEX);
+                MS3_DEBUG(F(" / 0x"));
+                MS3_DEBUG_AS(Queue::items[0].operation, HEX);
+                MS3_DEBUG(F(" / 0x"));
+                MS3_DEBUG_AS(Queue::items[0].data, HEX);
+                MS3_DEBUGLN();
 
-            queue[writePointer].address = address;
-            queue[writePointer].data = data;
-            queue[writePointer].dataLength = dataLength;
-            queue[writePointer].operation = operation;
+                // There it goes!
+                Queue::shift();
+            }
 
-            // Wrap pointer.
-            writePointer = (writePointer < MS3_QUEUE_SIZE - 1) ? writePointer + 1 : 0;
+            MS3_DEBUG(F("Add item "));
+            MS3_DEBUG(Queue::writePointer);
+            MS3_DEBUG(F(" to the queue: 0x"));
+            MS3_DEBUG_AS(address, HEX);
+            MS3_DEBUG(F(" / 0x"));
+            MS3_DEBUG_AS(operation, HEX);
+            MS3_DEBUG(F(" / 0x"));
+            MS3_DEBUG_AS(data, HEX);
+            MS3_DEBUGLN();
+
+            Queue::items[writePointer].address = address;
+            Queue::items[writePointer].data = data;
+            Queue::items[writePointer].dataLength = dataLength;
+            Queue::items[writePointer].operation = operation;
+
+            Queue::writePointer++;
         }
 
         /**
          * Check if the queue is currently empty.
          */
         bool isEmpty() {
-            return (readPointer == writePointer);
+            return (Queue::writePointer == 0);
         }
 };
 
